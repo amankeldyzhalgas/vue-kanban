1e6d913052ab34e5f751ef6ba28b2390c1536f7d
Zhalgas Amankeldy <amankeldy.zhalgas@gmail.com>
2021-05-10 18:17:30 +0600

    added list of tasks

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,53 @@
     <div class="row">
       <div class="col form-inline">
           <b-form-input v-model="newTask" placeholder="Enter task" @keyup.enter="add"></b-form-input>
+          <b-button class="ml-2" variant="primary" @click="add">Add</b-button>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <div class="col-md-3">
+        <div class="p-2 alert alert-secondary">
+          <h3>Backlog</h3>
+          <draggable class="list-group kanban-column" :list="arrBacklog" group="tasks">
+            <div class="list-group-item" v-for="element in arrBacklog" :key="element.name">
+              {{element.name}}
+            </div>
+          </draggable>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="p-2 alert alert-primary">
+          <h3>In Progress</h3>
+          <draggable class="list-group kanban-column" :list="arrInProgress" group="tasks">
+            <div class="list-group-item" v-for="element in arrInProgress" :key="element.name">
+              {{element.name}}
+            </div>
+          </draggable>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="p-2 alert alert-warning">
+          <h3>Tested</h3>
+          <draggable class="list-group kanban-column" :list="arrTested" group="tasks">
+            <div class="list-group-item" v-for="element in arrTested" :key="element.name">
+              {{element.name}}
+            </div>
+          </draggable>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="p-2 alert alert-success">
+          <h3>Done</h3>
+          <draggable class="list-group kanban-column" :list="arrDone" group="tasks">
+            <div class="list-group-item" v-for="element in arrDone" :key="element.name">
+              {{element.name}}
+            </div>
+          </draggable>
+        </div>
       </div>
     </div>
   </div>
@@ -26,15 +73,17 @@ export default {
         {name: "test3"},
       ],
       arrInProgress: [],
-      arrTestes: [],
+      arrTested: [],
       arrDone: []
     }
   },
   methods: {
     add() {
-      if (this.newTask){
+      if (this.newTask && !this.arrBacklog.some(x => x.name === this.newTask)){
         this.arrBacklog.push({name: this.newTask});
         this.newTask="";
+      } else {
+        alert("invalid value");
       }
     }
   }
@@ -42,4 +91,7 @@ export default {
 </script>
 
 <style>
+.kanban-column {
+  min-height: 300px;
+}
 </style>
